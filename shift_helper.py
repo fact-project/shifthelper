43f992269b8f7822280b7adddaf9a12e66c82a5a
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-07-06 17:30:35 +0100

    add missing imports

--- shift_helper.py
+++ shift_helper.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 from __future__ import print_function
 import time
+from datetime import datetime
+from blessings import Terminal
+term = Terminal()
 import handle_QLA
 import handle_dim_stuff
 import handle_cmd_line_UI
@@ -9,7 +12,9 @@ import handle_Skype
 
 handle_dim_stuff.setup()
 handle_QLA.setup()
+
 my_phone_number = handle_cmd_line_UI.get_tested_phone_number()
+
 delay_between_checks = 60  # in sec.
 assert handle_Skype.phone_ringing_time < delay_between_checks
 while True:
