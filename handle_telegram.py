1f20777b2685555fc1bc677d81730cca6154acc7
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-07-09 22:08:45 +0200

    add timeout to requests and use dict.get properly

--- handle_telegram.py
+++ handle_telegram.py
@@ -12,17 +12,14 @@ chat_id = None
 
 
 def get_last_message_data():
-    update = requests.get(url.format(method='getUpdates'))
+    update = requests.get(url.format(method='getUpdates'), timeout=1)
     update = json.loads(update.content.decode('utf8'))
 
     if update['ok']:
         chatdata = update['result'][-1]['message']['chat']
         chat_id = chatdata['id']
         firstname = chatdata['first_name']
-        if 'lastname' in chatdata:
-            lastname = chatdata['last_name']
-        else:
-            lastname = ''
+        lastname = chatdata.get('last_name', '')
         return chat_id, firstname, lastname
 
 
@@ -30,6 +27,7 @@ def send_message(message):
     r = requests.post(
         url.format(token=bot_token, method='sendMessage'),
         data={'chat_id': chat_id, 'text': message},
+        timeout=1,
     )
     return r
 
