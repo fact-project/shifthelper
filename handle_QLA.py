25f53e3d2949fe3a3cbf774ecff8a358ce9bed9e
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-07-25 00:59:12 +0200

    first try in replicating QLA binning

--- handle_QLA.py
+++ handle_QLA.py
@@ -40,14 +40,17 @@ factdb = create_engine(
 )
 
 
-def get_max_rates():
+def get_data(bin_width_minutes=20):
     ''' this will get the QLA results to call if you have to send an alert '''
     keys = [
         'QLA.fRunID',
         'QLA.fNight',
         'QLA.fNumExcEvts',
+        'QLA.fNumSigEvts',
+        'QLA.fNumBgEvts',
         'QLA.fOnTimeAfterCuts',
         'RunInfo.fRunStart',
+        'RunInfo.fRunStop',
         'Source.fSourceName',
         'Source.fSourceKEY',
     ]
@@ -62,43 +65,40 @@ def get_max_rates():
     """
     sql_query = sql_query.format(
         comma_sep_keys=', '.join(keys),
-        night=fact.night_integer(),
+        night=20150723,
     )
 
-    data = pd.read_sql_query(sql_query, factdb, parse_dates=['fRunStart'])
+    data = pd.read_sql_query(
+        sql_query,
+        factdb,
+        parse_dates=['fRunStart', 'fRunStop'],
+    )
     # drop rows with NaNs from the table, these are unfinished qla results
     data.dropna(inplace=True)
 
     # if no qla data is available, return None
     if len(data.index) == 0:
         return None
-    data.set_index('fRunStart', inplace=True)
+
     # group by source to do the analysis seperated for each one
     grouped = data.groupby('fSourceName')
-    # resample in 20 min intervals by summing up events and ontime
-    binned = grouped.resample(
-        '20Min',
-        how={
-            'fNumExcEvts': 'sum',
+    binned = pd.DataFrame()
+    for source, group in grouped:
+        group['bin'] = dorner_binning(group, bin_width_minutes)
+        agg = group.groupby('bin').aggregate({
             'fOnTimeAfterCuts': 'sum',
-            'fSourceKEY': 'median',
-        },
-    )
-    # throw away bins with less than 5 minutes of datataking
-    binned = binned.query('fOnTimeAfterCuts >= 300').copy()
-    if len(binned.index) == 0:
-        return None
-
-    # calculate excess events per hour
-    binned['rate'] = binned.fNumExcEvts / binned.fOnTimeAfterCuts * 3600
-
-    create_bokeh_plot(binned)
-
-    # get the maximum rate for each source
-    max_rate = binned.groupby(level='fSourceName').aggregate(
-        {'rate': 'max', 'fSourceKEY': 'median'}
-    )
-    return max_rate
+            'fNumExcEvts':      'sum',
+            'fNumSigEvts':      'sum',
+            'fNumBgEvts':       'sum',
+            'fRunStart':        'min',
+            'fRunStop':         'max',
+            'fSourceKEY':       'median',
+        })
+        agg['fSourceName'] = source
+        agg['rate'] = agg.fNumExcEvts / agg.fOnTimeAfterCuts * 3600
+        valid = agg.query('fOnTimeAfterCuts > 0.9*60*{}'.format(bin_width_minutes))
+        binned = binned.append(valid, ignore_index=True)
+    return binned
 
 
 def create_bokeh_plot(data):
@@ -107,17 +107,32 @@ def create_bokeh_plot(data):
     '''
     output_file('plots/qla.html', title='ShiftHelper QLA')
     fig = figure(width=600, height=400, x_axis_type='datetime')
-    for i, source in enumerate(data.index.levels[0]):
+    for i, (name, group) in enumerate(data.groupby('fSourceName')):
         fig.circle(
-            x=data.loc[source].index.values,
-            y=data.loc[source].rate,
-            legend=source,
+            x=group.fRunStart,
+            y=group.rate,
+            legend=name,
             color=colors[i],
         )
     legend = fig.legend[0]
     legend.orientation = 'top_left'
     save(fig)
 
+
+def create_mpl_plot(data):
+    import matplotlib.pyplot as plt
+    for name, group in data.groupby('fSourceName'):
+        group.plot(
+            x='fRunStart',
+            y='rate',
+            label=name,
+            marker='x',
+            linestyle='none',
+            ax=plt.gca(),
+        )
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('plot.png')
 
 def perform_checks():
     """ raise ValueError if new flare detected.
@@ -126,10 +141,13 @@ def perform_checks():
     and also higher than before, throw a ValueErrorm which leads to
     a skype-call inside the main while loop of shift_helper.py
     """
-    qla_max_rates = get_max_rates()
-    if qla_max_rates is None:
+    data = get_data()
+    if data is None:
         print('No QLA data available yet')
         return
+    qla_max_rates = data.groupby('fSourceName').agg({'rate': 'max'})
+
+    create_mpl_plot(data)
 
     print('max rates of today:')
     for source, data in qla_max_rates.iterrows():
@@ -145,6 +163,7 @@ def perform_checks():
         print('{} : {:3.1f} Events/h'.format(source, max_rate[source]))
 
 def get_image(source_key):
+    ''' returns the png qla-plot of the selected source '''
     night = fact.night()
     link = 'http://fact-project.org/lightcurves/' \
         '{year}/{month:02d}/{day:02d}/' \
@@ -159,3 +178,18 @@ def get_image(source_key):
     with open('plots/qla.png', 'wb') as f:
         f.write(ret.read())
     return open('plots/qla.png', 'rb')
+
+
+def dorner_binning(data, bin_width_minutes=20):
+    ''' this hopefully creates the same binning as in lightcurve.C
+    '''
+    bin_number = 0
+    ontime_sum = 0
+    bins = []
+    for key, row in data.iterrows():
+        ontime_sum += row['fOnTimeAfterCuts']
+        bins.append(bin_number)
+        if ontime_sum > bin_width_minutes * 60:
+            bin_number += 1
+            ontime_sum = 0
+    return pd.Series(bins, index=data.index)
