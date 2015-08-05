0ccdeaede0e71dbaa40408ce182bf82d8da6492c
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-08-05 23:40:37 +0200

    minor fixes

--- cli/__init__.py
+++ cli/__init__.py
@@ -12,12 +12,12 @@ class StatusDisplay(Thread):
         self.status_data = status_data
         self.qla_data = qla_data
         self.term = Terminal()
-        self.term.clear()
         self.stop_event = stop_event
         super(StatusDisplay, self).__init__()
 
     def run(self):
         with self.term.fullscreen(), self.term.hidden_cursor():
+            print(self.term.clear())
             while not self.stop_event.is_set():
                 self.update_status()
                 self.stop_event.wait(1)
@@ -27,12 +27,12 @@ class StatusDisplay(Thread):
 
         print(self.term.move(2, 0) + 'System Status')
         for i, (key, val) in enumerate(self.status_data.iteritems()):
-            print(self.term.move(3+i, 0) + u'{:<20}  {:6>} {:<6}'.format(
+            print(self.term.move(3+i, 0) + u'{:<20}  {:>6} {:<6}'.format(
                 key, *val
             ))
 
         print(self.term.move(2, 40) + 'Maximum Source Activity')
         for i, (key, val) in enumerate(self.qla_data.iteritems()):
-            print(self.term.move(3+i, 40) + u'{:<20}  {:6>} {:<6}'.format(
+            print(self.term.move(3+i, 40) + u'{:<20}  {:>6} {:<6}'.format(
                 key, *val
             ))
