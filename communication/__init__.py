d20c97af5028a2b46c5a96afac5e77d88bdef343
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-09-11 16:21:39 +0200

    remove skype interface

--- communication/__init__.py
+++ communication/__init__.py
@@ -2,5 +2,4 @@
 from __future__ import absolute_import, print_function, division
 
 from .twilio_interface import TwilioInterface
-from .skype_interface import SkypeInterface
 from .telegram_interface import TelegramInterface
