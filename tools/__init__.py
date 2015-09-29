c31bbe3cc1d1b1fe77d0254a0de48da45a2866ff
Dominik Neise <dominik.neise@udo.edu>
2015-09-29 14:23:50 +0200

    adding read_config_file to tools. removed from M import X, because this obscures a bit, where things come from in the code.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,14 +1,15 @@
-from datetime import datetime
-from datetime import timedelta
+import os
+import datetime
+from ConfigParser import SafeConfigParser
 
 def night(timestamp=None):
     """
     gives the date for a day change at noon instead of midnight
     """
     if timestamp is None:
-        timestamp = datetime.utcnow()
+        timestamp = datetime.datetime.utcnow()
     if timestamp.hour < 12:
-        timestamp = timestamp - timedelta(days=1)
+        timestamp = timestamp - datetime.timedelta(days=1)
     return timestamp
 
 
@@ -18,8 +19,22 @@ def night_integer(timestamp=None):
         of yesterday
     """
     if timestamp is None:
-        timestamp = datetime.utcnow()
+        timestamp = datetime.datetime.utcnow()
     if timestamp.hour < 12:
-        timestamp = timestamp - timedelta(days=1)
+        timestamp = timestamp - datetime.timedelta(days=1)
     night = int(timestamp.strftime('%Y%m%d'))
     return night
+
+def read_config_file(config_file_name):
+    if not os.path.isfile(config_file_name):
+        raise IOError('\n'
+            '\tYou need to decrypt the config file using: \n'
+            '\t$ gpg -o config.ini --decrypt config.gpg \n'
+            '\tYou will be asked for a password, enter the new FACT password'
+        )
+
+    config = SafeConfigParser()
+    list_of_successfully_parsed_files = config.read(config_file_name)
+    if config_file_name not in list_of_successfully_parsed_files:
+        raise Exception('Can not find a config file named: '+config_file_name)
+    return config
