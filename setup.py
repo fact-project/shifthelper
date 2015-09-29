61fbf2e3043c43fdfd81a9466a0a253caaf4eef6
Dominik Neise <dominik.neise@udo.edu>
2015-09-29 09:07:19 +0200

    adding first try of a setup.py ... not yet working!

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='fact_shift_helper',
+    version='0.1',
+    description='a tool for helping people with a FACT night shift',
+    url='https://bitbucket.org/dneise/fact_shift_helper',
+    author='Dominik Neise, Maximilian Noethe, Sebastian Mueller',
+    author_email='neised@phys.ethz.ch',
+    license='MIT',
+    packages=['fact_shift_helper'],
+    install_requires=[
+        'requests',         # 2.7.0 is in anaconda
+        'scikit-image',     # 0.11.3 is in anaconda
+        'numpy',            # 1.9.2 is in anaconda
+        'scipy',            # 0.15.1 is in anaconda
+        'matplotlib>=1.4',  # 1.4.3 is in anaconda
+        'python-dateutil',  # 2.4.2 is in anaconda
+        'pymongo>=2.7',     # 2.8 is in anaconda
+        'pandas',           # 0.16.2 is in anaconda
+        'sqlalchemy',       # 1.0.5 is in anaconda
+        'PyMySQL',          # 0.6.6 is in anaconda
+        'pytz',             # 2015.4 is in anaconda
+        'blessings',
+        'twilio',
+    ],
+    scripts=['scripts/xmlproc_parse', 'scripts/xmlproc_val'],
+    zip_safe=False
+)
