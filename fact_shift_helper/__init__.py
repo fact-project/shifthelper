195d64a8c218f1e2c3f1a4e00a14129d5c4a8e18
Dominik Neise <dominik.neise@udo.edu>
2015-10-06 10:18:31 +0200

    removing RelativeCameraHumidityCheck; adding version info

--- fact_shift_helper/__init__.py
+++ fact_shift_helper/__init__.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
-from .communication import TwilioInterface, TelegramInterface
+import pkg_resources
+__version__ = pkg_resources.require('fact_shift_helper')[0].version
+
+from .communication import TwilioInterface, TelegramInterface, NoCaller
 from .checks import Alert
 from .checks.qla import FlareAlert
 from .checks.webdim import RelativeCameraTemperatureCheck
-from .checks.webdim import RelativeCameraHumidityCheck
 from .checks.webdim import CurrentCheck
 from .checks.webdim import MainJsStatusCheck
 from .checks.webdim import WeatherCheck
 from .checks.computervision import LidCamCheck
 from .checks.computervision import IrCamCheck
 from .unused.handle_statistics import S_B_01, S_Li_Ma
+
