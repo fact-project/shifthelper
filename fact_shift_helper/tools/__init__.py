d2004c5f6ce44f793f1a3d0865038bcc1473ce8d
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-12-15 08:39:29 +0100

    add version tag to config.ini

--- fact_shift_helper/tools/__init__.py
+++ fact_shift_helper/tools/__init__.py
@@ -3,6 +3,19 @@ import datetime
 from six.moves.configparser import SafeConfigParser
 import pkg_resources
 import shutil
+from fact_shift_helper import __version__
+
+config_file = os.path.join(
+    os.environ['HOME'],
+    '.shift_helper/config-{}.ini'.format(__version__)
+)
+
+decrypt_message = '''\n
+\tYou need to decrypt the config file using: \n
+\t$ cd ~/.shifthelper \n
+\t$ gpg -o config-{}.ini --decrypt config-{}.gpg \n
+\tYou will be asked for a password, enter the new FACT password'
+'''.format(__version__)
 
 
 def night(timestamp=None):
@@ -29,28 +42,23 @@ def night_integer(timestamp=None):
     return night
 
 
-def read_config_file(config_file_name):
-    if os.path.isfile(config_file_name):
+def read_config_file():
+    if os.path.isfile(config_file):
 
         config = SafeConfigParser()
         config.optionxform = str
-        list_of_successfully_parsed_files = config.read(config_file_name)
-        if config_file_name not in list_of_successfully_parsed_files:
+        list_of_successfully_parsed_files = config.read(config_file)
+        if config_file not in list_of_successfully_parsed_files:
             raise Exception(
-                'Could not read {0} succesfully.'.format(config_file_name)
+                'Could not read {0} succesfully.'.format(config_file)
             )
         return config
 
     else:
         shutil.copyfile(
             src=pkg_resources.resource_filename(__name__, 'config.gpg'),
-            dst=config_file_name.replace('ini', 'gpg'),
+            dst=config_file.replace('ini', 'gpg'),
         )
 
         raise IOError(
-            '\n'
-            '\tYou need to decrypt the config file using: \n'
-            '\t$ cd ~/.shifthelper \n'
-            '\t$ gpg -o config.ini --decrypt config.gpg \n'
-            '\tYou will be asked for a password, enter the new FACT password'
         )
