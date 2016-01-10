38dbbc9b8a6ecc9ac9d8b6d8bb82f08b5f2ddbd0
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-01-10 13:46:41 +0100

    Set significance to zero for negative excess rates, fix error for single numbers

--- fact_shift_helper/checks/qla.py
+++ fact_shift_helper/checks/qla.py
@@ -223,20 +223,20 @@ def dorner_binning(data, bin_width_minutes=20):
 
 
 def li_ma_significance(N_on, N_off, alpha=0.2):
-    N_on = np.array(N_on, copy=False)
-    N_off = np.array(N_off, copy=False)
+    N_on = np.array(N_on, copy=False, ndmin=1)
+    N_off = np.array(N_off, copy=False, ndmin=1)
 
     with np.errstate(divide='ignore', invalid='ignore'):
-        p1 = N_on / (N_on + N_off)
-        p2 = N_off / (N_on + N_off)
+        p_on = N_on / (N_on + N_off)
+        p_off = N_off / (N_on + N_off)
 
-        t1 = N_on * np.log(((1 + alpha) / alpha) * p1)
-        t2 = N_off * np.log((1 + alpha) * p2)
+        t1 = N_on * np.log(((1 + alpha) / alpha) * p_on)
+        t2 = N_off * np.log((1 + alpha) * p_off)
 
         ts = (t1 + t2)
-
         significance = np.sqrt(ts * 2)
 
     significance[np.isnan(significance)] = 0
+    significance[N_on < alpha * N_off] = 0
 
     return significance
