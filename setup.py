2b2affad9b63998d8e3051c167dd48074d9a56b0
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-01-13 10:04:33 +0100

    new version 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fact_shift_helper',
-    version='0.3.3',
+    version='0.3.4',
     description='a tool for helping people with a FACT night shift',
     url='https://bitbucket.org/dneise/fact_shift_helper',
     author='Dominik Neise, Maximilian Noethe, Sebastian Mueller',
