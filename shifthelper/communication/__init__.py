675fc063dc7fb69543585cac4f1999511d08965a
Dominik Neise <neised@phys.ethz.ch>
2016-02-24 17:59:28 +0100

    add PlivoInterface

--- shifthelper/communication/__init__.py
+++ shifthelper/communication/__init__.py
@@ -2,5 +2,6 @@
 from __future__ import absolute_import, print_function, division
 
 from .twilio_interface import TwilioInterface
+from .plivo_interface import PlivoInterface
 from .telegram_interface import TelegramInterface
 from .caller import NoCaller
