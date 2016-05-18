db18b2420414cbafbcbbd35eceab42238fd7807f
Dominik Neise <neised@phys.ethz.ch>
2016-05-18 15:47:21 +0200

    just version change, config file on server was changed to reflect updated current limits

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shifthelper',
-    version='0.6.0',
+    version='0.6.1',
     description='a tool for helping people with a FACT night shift',
     url='https://github.com/fact-project/shifthelper',
     author='Dominik Neise, Maximilian Noethe, Sebastian Mueller',
