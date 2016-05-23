aaaac05d2a61c29d55d59120659a18cb03be8a80
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-05-23 16:42:50 +0200

    Add option for ringtime

--- shifthelper/tools/call.py
+++ shifthelper/tools/call.py
@@ -3,10 +3,11 @@
 '''
 This script uses the fact twilio account to call a number.
 
-Logs are stored in ~/.shifthelper
-
 Usage:
     fact_call <phone_number> [options]
+
+Options:
+    --ringtime=<N>   ringtime in seconds, defaults to value in the config file
 '''
 from docopt import docopt
 import pkg_resources
@@ -24,9 +25,14 @@ def main():
     )
     config = read_config_file()
 
+    if args['--ringtime']:
+        ringtime = int(args['--ringtime'])
+    else:
+        ringtime = config.getint('caller', 'ringtime')
+
     caller = Caller(
         args['<phone_number>'],
-        config.getint('caller', 'ringtime'),
+        ringtime,
         config.get('twilio', 'sid'),
         config.get('twilio', 'auth_token'),
         config.get('twilio', 'number'),
