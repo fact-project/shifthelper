22254f58098675f466d83621492467bbf4864179
Dominik Neise <neised@phys.ethz.ch>
2016-06-01 18:57:30 +0200

    use config.py get rid of parser; move create_db into tools

--- shifthelper/tools/__init__.py
+++ shifthelper/tools/__init__.py
@@ -5,17 +5,10 @@ import requests
 from getpass import getpass
 import sys
 import pkg_resources
+import sqlalchemy
+from ..config import config
 
 __version__ = pkg_resources.require('shifthelper')[0].version
-
-config_file_name = 'config-{}.ini'.format(__version__)
-config_file_path = os.path.join(
-    os.environ['HOME'], '.shifthelper', config_file_name
-)
-
-remote_config_url = "https://fact-project.org/sandbox/shifthelper/config/{cn}".format(
-    cn=config_file_name
-)
 
 
 def night(timestamp=None):
@@ -42,39 +35,14 @@ def night_integer(timestamp=None):
     return night
 
 
-def download_config_file():
-    dotpath = os.path.join(os.environ['HOME'], '.shifthelper')
-    if not os.path.exists(dotpath):
-        os.makedirs(dotpath)
-
-    passwd = getpass('Please enter the current FACT password')
-
-    try:
-        ret = requests.get(
-            remote_config_url, auth=('FACT', passwd), verify=False
+def create_db_connection():
+    db_config = config['database']
+    factdb = sqlalchemy.create_engine(
+        "mysql+pymysql://{user}:{pw}@{host}/{db}".format(
+            user = db_config['user'],
+            pw = db_config['password'],
+            host = db_config['host'],
+            db = db_config['database'],
         )
-        ret.raise_for_status()
-    except requests.RequestException:
-        if ret.status_code == 401:
-            print('Wrong password')
-        else:
-            print('Could not download config file from fact-project.org')
-        sys.exit()
-
-    else:
-        with open(config_file_path, 'wb') as f:
-            f.write(ret.content)
-
-
-def read_config_file():
-    if not os.path.isfile(config_file_path):
-        download_config_file()
-
-    config = SafeConfigParser()
-    config.optionxform = str
-    list_of_successfully_parsed_files = config.read(config_file_path)
-    if config_file_path not in list_of_successfully_parsed_files:
-        raise Exception(
-            'Could not read {0} succesfully.'.format(config_file_path)
-        )
-    return config
+    )
+    return factdb
