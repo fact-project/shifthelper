1c6bda61c6db96a949ea1f1e75119db4b2bea6b4
Dominik Neise <neised@phys.ethz.ch>
2016-06-01 19:03:25 +0200

    remove unneeded user interaction

--- shifthelper/communication/caller.py
+++ shifthelper/communication/caller.py
@@ -4,7 +4,7 @@ from ..cli import enter_phone_number, ask_user
 
 
 class Caller(object):
-    def __init__(self, phone_number, ring_time):
+    def __init__(self, phone_number, ring_time=20):
         self.ring_time = ring_time
         self.phone_number = phone_number
 
@@ -14,25 +14,6 @@ class Caller(object):
     def place_call(self):
         raise NotImplementedError
 
-    def check_phone_number(self):
-        calling_worked = False
-        while (not calling_worked or self.phone_number is None):
-            if self.phone_number is None:
-                self.phone_number = enter_phone_number()
-            self.confirm_phone_number()
-
-            if self.phone_number is not None:
-                print('I will try to call you now')
-                self.place_call()
-
-                if ask_user('Did your phone ring?'):
-                    self.hangup()
-                    calling_worked = True
-
-    def confirm_phone_number(self):
-        print('You entered: ', self.phone_number)
-        if not ask_user('Is that number correct?'):
-            self.phone_number = None
 
 
 class NoCaller(Caller):
@@ -47,8 +28,3 @@ class NoCaller(Caller):
     def place_call(self):
         pass
 
-    def check_phone_number(self):
-        pass
-
-    def confirm_phone_number(self):
-        pass
