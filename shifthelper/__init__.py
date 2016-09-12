151d6ef47f65e5dacfe2bdb48d0e365ebf6e2622
Dominik Neise <neised@phys.ethz.ch>
2016-09-12 13:50:11 +0200

    remove log config from init

--- shifthelper/__init__.py
+++ shifthelper/__init__.py
@@ -6,26 +6,3 @@ import logging.handlers
 import time
 import os
 from . import tools
-
-logdir = os.path.join(os.environ['HOME'], '.shifthelper')
-os.makedirs(logdir, exist_ok=True)
-
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-logfile_path = os.path.join(logdir, 'shifthelper.log')
-logfile_handler = logging.handlers.TimedRotatingFileHandler(
-    filename=logfile_path, 
-    when='D', interval=1,  # roll over every day.
-    backupCount=300,       # keep 300 days back log
-    utc=True
-)
-logfile_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    fmt='%(asctime)s - %(levelname)s - %(name)s | %(message)s',
-    #datefmt='%Y-%m-%dT%H:%M:%S',
-)
-formatter.converter = time.gmtime  # use utc in log
-logfile_handler.setFormatter(formatter)
-log.addHandler(logfile_handler)
-logging.getLogger('py.warnings').addHandler(logfile_handler)
-logging.captureWarnings(True)
