9c04f04da8fce5bf7afbdec957936000b16cc065
Dominik Neise <neised@phys.ethz.ch>
2016-09-14 19:22:03 +0200

    checkers look at website

--- shifthelper/__main__.py
+++ shifthelper/__main__.py
@@ -3,6 +3,7 @@
 import sys, json
 from custos import Custos, levels
 from custos import TelegramNotifier, LogNotifier, IntervalCheck
+from custos import HTTPNotifier
 from .notifiers import FactTwilioNotifier
 
 from . import checks
@@ -10,11 +11,36 @@ from .tools.whosonshift import whoisonshift
 from .tools import config
 from .logging import config_logging
 
-config_logging(to_console=True)
+config_logging(to_console=False)
 
 def telegram_book(category):
     return ['123665317']
     return [whoisonshift().iloc[0].telegram_id]
+
+twilio = FactTwilioNotifier(
+                    sid=config['twilio']['sid'],
+                    auth_token=config['twilio']['auth_token'],
+                    twilio_number=config['twilio']['number'],
+                    ring_time=10,
+                    level=levels.WARNING,
+                )
+telegram = TelegramNotifier(
+                    token=config['telegram']['token'],
+                    recipients=telegram_book,
+                    level=levels.INFO,
+                )
+http = HTTPNotifier(
+                    level=levels.WARNING,
+                    recipients=['http://localhost:5000/alerts'],
+                    auth=('fact', 'test'),
+                )
+
+log = LogNotifier(
+                    level=levels.DEBUG,
+                    recipients=['all'],
+                )
+
+
 
 def main():
     with Custos(
@@ -28,18 +54,10 @@ def main():
                 checks.RelativeCameraTemperatureCheck(interval=60),
             ],
             notifiers=[
-                FactTwilioNotifier(
-                    sid=config['twilio']['sid'],
-                    auth_token=config['twilio']['auth_token'],
-                    twilio_number=config['twilio']['number'],
-                    ring_time=10,
-                    level=levels.WARNING,
-                ),
-                TelegramNotifier(
-                    token=config['telegram']['token'],
-                    recipients=telegram_book,
-                    level=levels.INFO,
-                ),
+                twilio,
+                telegram,
+                http,
+                log,
             ],
             ) as custos:
         try:
