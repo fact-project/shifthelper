e7f003491c89afd8a2ff629f2d30b1e2313b438f
Dominik Neise <neised@phys.ethz.ch>
2016-09-15 09:33:11 +0200

    add new check:
     * DriveInErrorDuringDataRun
     * fix some typos
     * move boolean helper functions out of classes

--- shifthelper/checks.py
+++ shifthelper/checks.py
@@ -80,7 +80,70 @@ def is_parked():
     # should is_locked be taken into account here?
     # pointing north is enough ...
     # but driving through north ... is not :-|
-    return (-5 < az < 5) and (90 < zd < 180):
+    return (-5 < az < 5) and (90 < zd < 180)
+
+def is_drive_error():
+    drive_state = sfc.status()['Drive_control']
+    return drive_state in [
+        'ERROR',
+        'PositioningFailed',
+        'OutOfRange',
+        'InvalidCoordinates',
+        'FATAL',
+    ]
+
+def is_data_taking():
+    # if MCP::State::kTriggerOn ||MCP::State::kTakingData;
+    # the state name strings, I took from dimctrl on newdaq, typing `st`
+    return sfc.status()['MCP'] in ['TaTakingDatakingData', 'TriggerOn']
+
+def is_data_run():
+    # fMcpConfigurationName=='data' || fMcpConfigurationName=='data-rt';
+    # this fMcpConfigurationName seems to turn um in square brackets []
+    # inside sfc.main_page()['System_Status'], but I'm not sure yet.
+    # yes it does: example: 
+    # sfc.main_page()['System_Status'] --> 'Idle [single-pe]'
+    try:
+        config_name = re.search('\[(.*)\]', sfc.main_page()['System_Status']).groups()[0]
+        return config_name in ['data', 'data-rt']
+    except IndexError:
+        # regex did not match
+        return False
+
+def is_bias_not_operating():
+    ''' in smartfact.cc this check is done as:
+        fDimBiasControl.state() < BIAS::State::kRamping
+
+        this is the list of states with their numbers:
+        so we can explicitely describe, which ones we define as 'not operating'
+
+     -256: Offline
+       -1: NotReady (State machine not ready, events are ignored.)
+        0: Ready (State machine ready to receive events.)
+        1: Disconnected (Bias-power supply not connected via USB.)
+        2: Connecting (Trying to establish USB connection to bias-power supply.)
+        3: Initializing (USB connection to bias-power supply established, synchronizing USB stream.)
+        4: Connected (USB connection to bias-power supply established.)
+        5: Ramping (Voltage ramping in progress.)
+        6: OverCurrent (At least one channel is in over current state.)
+        7: VoltageOff (All voltages are supposed to be switched off.)
+        8: NotReferenced (Internal reference voltage does not match last sent voltage.)
+        9: VoltageOn (At least one voltage is switched on and all are at reference.)
+       10: ExpertMode (Special (risky!) mode to directly send command to the bias-power supply.)
+       11: Locked (Locked due to emergency shutdown, no commands accepted except UNLOCK.)
+      256: ERROR (Common error state.)
+    65535: FATAL (A fatal error occured, the eventloop is stopped.)
+    '''
+    bias_state = sfc.status()['Bias_control']
+    return bias_state in [
+        'Offline',
+        'NotReady',
+        'Ready',
+        'Disconnected',
+        'Connecting',
+        'Initializing',
+        'Connected',
+    ]
 
 class WindSpeedCheck(FactIntervalCheck):
     def inner_check(self):
@@ -110,65 +173,10 @@ class RelativeCameraTemperatureCheck(FactIntervalCheck):
 
 class BiasNotOperatingDuringDataRun(FactIntervalCheck):
     def inner_check(self):
-        if (self._is_bias_not_operating()
-                and self._is_data_taking()
-                and self._is_data_run()):
+        if (is_bias_not_operating()
+                and is_data_taking()
+                and is_data_run()):
             return 'Bias not operating during data run'
-
-    def _is_data_taking(self):
-        # if MCP::State::kTriggerOn ||MCP::State::kTakingData;
-        # the state name strings, I took from dimctrl on newdaq, typing `st`
-        return sfc.status()['MCP'] in ['TaTakingDatakingData', 'TriggerOn']
-
-    def _is_data_run(self)
-        # fMcpConfigurationName=='data' || fMcpConfigurationName=='data-rt';
-        # this fMcpConfigurationName seems to turn um in square brackets []
-        # inside sfc.main_page()['System_Status'], but I'm not sure yet.
-        # yes it does: example: 
-        # sfc.main_page()['System_Status'] --> 'Idle [single-pe]'
-        try:
-            config_name = re.search('\[(.*)\]', sfc.main_page()['System_Status']).groups()[0]
-            return config_name in ['data', 'data-rt']
-        except IndexError:
-            # regex did not match
-            return False
-
-    def _is_bias_not_operating(self):
-        ''' in smartfact.cc this check is done as:
-        fDimBiasControl.state() < BIAS::State::kRamping
-
-        this is the list of states with their numbers:
-        so we can explicitely describe, which ones we define as 'not operating'
-
-         -256: Offline
-           -1: NotReady (State machine not ready, events are ignored.)
-            0: Ready (State machine ready to receive events.)
-            1: Disconnected (Bias-power supply not connected via USB.)
-            2: Connecting (Trying to establish USB connection to bias-power supply.)
-            3: Initializing (USB connection to bias-power supply established, synchronizing USB stream.)
-            4: Connected (USB connection to bias-power supply established.)
-            5: Ramping (Voltage ramping in progress.)
-            6: OverCurrent (At least one channel is in over current state.)
-            7: VoltageOff (All voltages are supposed to be switched off.)
-            8: NotReferenced (Internal reference voltage does not match last sent voltage.)
-            9: VoltageOn (At least one voltage is switched on and all are at reference.)
-           10: ExpertMode (Special (risky!) mode to directly send command to the bias-power supply.)
-           11: Locked (Locked due to emergency shutdown, no commands accepted except UNLOCK.)
-          256: ERROR (Common error state.)
-        65535: FATAL (A fatal error occured, the eventloop is stopped.)
-
-
-        '''
-        bias_state = sfc.status()['Bias_control']
-        return bias_state in [
-            'Offline',
-            'NotReady',
-            'Ready',
-            'Disconnected',
-            'Connecting',
-            'Initializing',
-            'Connected',
-        ]
 
 
 class BiasChannelsInOverCurrent(FactIntervalCheck):
@@ -186,6 +194,14 @@ class BiasVoltageNotAtReference(FactIntervalCheck):
 
 
 class ContainerTooWarm(FactIntervalCheck):
-    def innter_check(self):
+    def inner_check(self):
         if sfc.container_temperature()['Current_temperature_in_C'] > 42:
             return 'Container Temperature above 42 deg C'
+
+
+class DriveInErrorDuringDataRun(FactIntervalCheck):
+    def inner_check(self):
+        if (is_drive_error()
+                and is_data_taking()
+                and is_data_run()):
+            return 'Drive in Error during Data run'
