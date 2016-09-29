9d9328e7f194844e5516b55e01c7ee916089dc9e
Dominik Neise <neised@phys.ethz.ch>
2016-09-29 11:05:20 +0200

    be able to create different db connections and specify port

--- shifthelper/tools/__init__.py
+++ shifthelper/tools/__init__.py
@@ -5,14 +5,18 @@ import json
 with open(os.path.join(os.environ['HOME'], '.shifthelper', 'config.json')) as f:
     config = json.load(f)
 
-def create_db_connection():
-    db_config = config['database']
+def create_db_connection(config=None):
+    if config is None:
+        db_config = config['database']
+    else:
+        db_config = config
     factdb = sqlalchemy.create_engine(
-        "mysql+pymysql://{user}:{pw}@{host}/{db}".format(
+        "mysql+pymysql://{user}:{pw}@{host}:{port}/{db}".format(
             user = db_config['user'],
             pw = db_config['password'],
             host = db_config['host'],
             db = db_config['database'],
+            port = db_config.get('port', 3306)
         )
     )
     return factdb
