da1b00405f1b945cee55fb1c58cb92a22a2ba9b4
Dominik Neise <neised@phys.ethz.ch>
2016-09-29 11:08:07 +0200

    adding a first example

--- shifthelper/db_cloner/__main__.py
+++ shifthelper/db_cloner/__main__.py
@@ -1,4 +1,17 @@
+from datetime import datetime
+import pandas as pd
 from ..tools import config
+from ..tools import create_db_connection
 
 def main():
-	print(config)
+    db_in = create_db_connection(config["database"])
+    db_out = create_db_connection(config["cloned_db"])
+    yesterday_night = (datetime.utcnow() - timedelta(hours=12)).date()
+    query = "SELECT u from calendar.Data where y={y} and m={m} and d={d}".format(
+                    y=yesterday_night.year, 
+                    m=yesterday_night.month - 1,
+                    d=yesterday_night.day
+                )
+    calendar_data = pd.read_sql_query(query, db_in)
+    calendar_data.to_sql("calendar_data", db_out, if_exists="replace")
+
