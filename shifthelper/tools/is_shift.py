cbe863dcad63a0a8277ffeb9a53ad1555076070a
Dominik Neise <neised@phys.ethz.ch>
2016-09-29 13:39:21 +0200

    using cloned_db in tools

--- shifthelper/tools/is_shift.py
+++ shifthelper/tools/is_shift.py
@@ -8,8 +8,8 @@ from datetime import timedelta
 @lru_cache(1)
 def get_MeasurementType(db=None):
     if db is None:
-        db = tools.create_db_connection()
-    df = pd.read_sql_query("select * from factdata.MeasurementType", db)
+        db = tools.create_db_connection(tools.config['cloned_db'])
+    df = pd.read_sql_query("select * from factdata_MeasurementType", db)
     df.set_index("fMeasurementTypeName", inplace=True)
     return df
 
@@ -17,11 +17,11 @@ def get_last_startup_or_shutdown(current_time_rounded_to_seconds=None, db=None):
     if current_time_rounded_to_seconds is None:
         current_time_rounded_to_seconds = datetime.utcnow().replace(microsecond=0)
     if db is None:
-        db = tools.create_db_connection()
+        db = tools.create_db_connection(tools.config['cloned_db'])
 
     types = get_MeasurementType(db)
     query = """
-    SELECT * FROM factdata.Schedule AS S 
+    SELECT * FROM factdata_Schedule AS S 
     WHERE 
         S.fMeasurementTypeKey IN {keys}
     AND
