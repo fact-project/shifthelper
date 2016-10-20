328715e78ca53beb1467f03e76bf5540a47eaab5
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-10-20 11:18:11 +0200

    Use json formatter for logfiles

--- shifthelper/logging.py
+++ shifthelper/logging.py
@@ -3,6 +3,9 @@ import sys
 import os
 import time
 
+from pythonjsonlogger.jsonlogger import JsonFormatter
+
+
 def config_logging(to_console=False):
     dot_shifthelper_dir = os.path.join(os.environ['HOME'], '.shifthelper')
     os.makedirs(dot_shifthelper_dir, exist_ok=True)
@@ -11,12 +14,10 @@ def config_logging(to_console=False):
         filename=os.path.join(dot_shifthelper_dir, 'shifthelper.log'), 
         when='D', interval=1,  # roll over every day.
         backupCount=300,       # keep 300 days back log
-        utc=True
+        utc=True,
     )
     logfile_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        fmt='%(asctime)s - %(levelname)s - %(name)s | %(message)s',
-    )
+    formatter = JsonFormatter()
     formatter.converter = time.gmtime  # use utc in log
     logfile_handler.setFormatter(formatter)
 
