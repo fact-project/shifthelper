08f23ea3dc00e15dcf1da75bbe2cfb7f54497d4f
Dominik Neise <neised@phys.ethz.ch>
2016-10-27 09:24:14 +0200

    pep8

--- shifthelper/db_cloner/__main__.py
+++ shifthelper/db_cloner/__main__.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from datetime import datetime, timedelta
 import pandas as pd
 from ..tools import config
@@ -10,38 +10,41 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 logging.config.fileConfig(os.path.join(dir_path, 'logging.conf'))
 log = logging.getLogger(__name__)
 
+
 def factdata_MeasurementType():
     return """SELECT * from factdata.MeasurementType"""
-        
+
+
 def calendar_data():
     yesterday_night = (datetime.utcnow() - timedelta(hours=12)).date()
     return """
-        SELECT * 
-        FROM calendar.Data 
-        WHERE y={y} 
-            AND m={m} 
+        SELECT *
+        FROM calendar.Data
+        WHERE y={y}
+            AND m={m}
             AND d={d}
         """.format(
-                y=yesterday_night.year, 
-                m=yesterday_night.month - 1,
-                d=yesterday_night.day
-            )
+            y=yesterday_night.year,
+            m=yesterday_night.month - 1,
+            d=yesterday_night.day
+        )
+
 
 def factdata_Schedule():
     return """
-        SELECT * 
-        FROM factdata.Schedule AS S 
+        SELECT *
+        FROM factdata.Schedule AS S
         WHERE
             S.fStart > "{one_month_ago}"
-    """.format(one_month_ago = (datetime.utcnow() - timedelta(days=30)))
-    
+    """.format(one_month_ago=(datetime.utcnow() - timedelta(days=30)))
+
 
 def users():
     return """
-        SELECT * from logbook.users 
-        JOIN 
-            logbook.userfields 
-        ON 
+        SELECT * from logbook.users
+        JOIN
+            logbook.userfields
+        ON
             logbook.users.uid=logbook.userfields.ufid
         """
 
@@ -53,11 +56,15 @@ def main():
     while True:
         try:
             log.info("clonging ...")
-            for query_func in [factdata_MeasurementType, calendar_data, factdata_Schedule, users]:
+            for query_func in [
+                    factdata_MeasurementType,
+                    calendar_data,
+                    factdata_Schedule,
+                    users]:
                 table = pd.read_sql_query(query_func(), db_in)
                 table.to_sql(query_func.__name__, db_out, if_exists="replace")
             log.info("...done")
-            time.sleep(1 * 60) # 15 minutes
+            time.sleep(1 * 60)  # 15 minutes
         except (SystemExit, KeyboardInterrupt):
             break
         except:
