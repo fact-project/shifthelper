577a9f73315fe1b797f808501fbdc0344c44c29d
Dominik Neise <neised@phys.ethz.ch>
2016-10-27 10:31:11 +0200

    pep8

--- shifthelper/tools/is_shift.py
+++ shifthelper/tools/is_shift.py
@@ -2,9 +2,8 @@ import pandas as pd
 from .. import tools
 from functools import lru_cache
 from datetime import datetime
-from datetime import timedelta
-
 from retrying import retry
+
 
 @lru_cache(1)
 def get_MeasurementType(db=None):
@@ -14,6 +13,7 @@ def get_MeasurementType(db=None):
     df.set_index("fMeasurementTypeName", inplace=True)
     return df
 
+
 def get_last_startup_or_shutdown(current_time_rounded_to_seconds=None, db=None):
     if current_time_rounded_to_seconds is None:
         current_time_rounded_to_seconds = datetime.utcnow().replace(microsecond=0)
@@ -22,28 +22,30 @@ def get_last_startup_or_shutdown(current_time_rounded_to_seconds=None, db=None):
 
     types = get_MeasurementType(db)
     query = """
-    SELECT * FROM factdata_Schedule AS S 
-    WHERE 
+    SELECT * FROM factdata_Schedule AS S
+    WHERE
         S.fMeasurementTypeKey IN {keys}
     AND
         S.fStart < "{now}"
     ORDER BY S.fStart DESC
     LIMIT 1
     """.format(
-        keys=(types.loc["Startup"].fMeasurementTypeKey,
+        keys=(
+            types.loc["Startup"].fMeasurementTypeKey,
             types.loc["Shutdown"].fMeasurementTypeKey),
         now=current_time_rounded_to_seconds
         )
-    
+
     return pd.merge(
         pd.read_sql_query(query, db),
-        types.reset_index(), 
+        types.reset_index(),
         on="fMeasurementTypeKey"
     )
 
-@retry(stop_max_delay=30000, # 30 seconds max  
-       wait_exponential_multiplier=100, # wait 2^i * 100 ms, on the i-th retry
-       wait_exponential_max=1000, # but wait 1 second per try maximum 
+
+@retry(stop_max_delay=30000,  # 30 seconds max
+       wait_exponential_multiplier=100,  # wait 2^i * 100 ms, on the i-th retry
+       wait_exponential_max=1000,  # but wait 1 second per try maximum
        )
 def is_shift_at_the_moment(time=None):
     if time is None:
