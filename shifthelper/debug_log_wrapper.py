fc81e19e0e8c712e130a034d28785ae841cae765
Dominik Neise <neised@phys.ethz.ch>
2016-11-04 15:53:33 +0100

    tabs to spaces

--- shifthelper/debug_log_wrapper.py
+++ shifthelper/debug_log_wrapper.py
@@ -5,7 +5,7 @@ import logging
 
 @decorator
 def log_call_and_result(wrapped, instance, args, kwargs):
-	log = logging.getLogger(getmodule(wrapped).__name__)
-	result = wrapped(*args, **kwargs)
-	log.debug('%s -> %s', wrapped.__name__, result)
-	return result
+    log = logging.getLogger(getmodule(wrapped).__name__)
+    result = wrapped(*args, **kwargs)
+    log.debug('%s -> %s', wrapped.__name__, result)
+    return result
