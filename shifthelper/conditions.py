ad9b603360b8261772f7ea2faf0e4889085ed38c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-01-11 09:26:37 +0100

    Better log message

--- shifthelper/conditions.py
+++ shifthelper/conditions.py
@@ -287,7 +287,7 @@ def is_dummy_alert_by_shifter():
             try:
                 current_shifter = get_current_shifter().username
             except IndexError:
-                log.error('Could not get current shifter')
+                log.debug('Nobody on shift')
                 return False
             log.debug('current shifter is: %s', current_shifter)
             if current_shifter != username:
