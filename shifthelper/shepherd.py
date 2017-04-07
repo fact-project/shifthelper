cb2e63113951ce5b9e28a7954755d0cab346e18e
Dominik Neise <neised@phys.ethz.ch>
2017-04-07 13:08:37 +0200

    add shepherd.py (to be console script)

---
+++ shifthelper/shepherd.py
@@ -0,0 +1,39 @@
+import pytz
+import requests
+import dateutil
+import dateutil.parser
+from datetime import datetime
+from twilio.rest import RestClient as Client
+from .tools import config
+
+c = config
+
+url = 'https://ihp-pc41.ethz.ch/time'
+TIMEOUT = 10 * 60  # timeout in seconds
+hangup_twiml = '''<Response><Hangup/></Response>'''
+hangup_url = build_url(echo_url, {'Twiml': hangup_twiml})
+
+
+def check_server_time():
+    try:
+        server_time = dateutil.parser.parse(
+            requests.get(url).json()['time']
+        )
+        delta = datetime.now(pytz.utc) - server_time
+        if delta.total_seconds() > TIMEOUT:
+            raise Exception('servertime too old')
+    except:
+        Client(
+            c['twilio']['sid'],
+            c['twilio']['auth_token']
+            ).calls.create(
+                to=c['developer']['phone_number'],
+                from_=c['twilio']['number'],
+                url=hangup_url,
+                timeout=20,
+        )
+
+if __name__ == "__main__":
+    while True:
+        check_server_time()
+        time.sleep(60)
