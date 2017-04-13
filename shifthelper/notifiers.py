844a748bd6cab2e9665c0bca5a86ae8b531e65ea
Dominik Neise <neised@phys.ethz.ch>
2017-04-13 10:18:04 +0200

    put `is_alert_acknowledged` here where it is needed
    this reduces tools/__init__.py a bit, which I always like,
    since tools are somehow confusing, as one never knows where they are
    needed.

--- shifthelper/notifiers.py
+++ shifthelper/notifiers.py
@@ -1,7 +1,8 @@
+import requests
+from retrying import retry
 import datetime
 from .tools.shift import get_current_shifter
 from copy import copy
-from .tools import config, is_alert_acknowledged
 from .categories import CATEGORY_DEVELOPER
 
 import logging
@@ -87,4 +88,22 @@ class NotAcknowledgedMessagePseudoNotifier:
 
     def phone_number_of_developer(self):
         return config['developer']['phone_number']
+
+
+@retry(stop_max_delay=30000,  # 30 seconds max
+       wait_exponential_multiplier=100,  # wait 2^i * 100 ms, on the i-th retry
+       wait_exponential_max=1000,  # but wait 1 second per try maximum
+       wrap_exception=True,
+       )
+@cached(cache=TTLCache(1, ttl=30))
+def get_alerts():
+    return requests.get(config['webservice']['post-url']).json()
+
+
+def is_alert_acknowledged(self, uuid):
+    try:
+        alerts = get_alerts()
+        return alerts[uuid]['acknowledged']
+    except (IndexError, TypeError, RetryError):
+        return False
 
