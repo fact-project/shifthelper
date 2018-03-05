f7adb4334f5e214deed442bdf12b02973bbe7c61
Dominik Neise <neised@phys.ethz.ch>
2018-03-05 20:51:07 +0100

    should fix it

--- shifthelper/checks.py
+++ shifthelper/checks.py
@@ -154,7 +154,7 @@ def all_recent_alerts_acknowledged(
     If there are no alerts matching the specified criteria, the result
     is dependent on the `result_if_no_alerts` option, which defaults to `False`
     '''
-    now = datetime.utcnow()
+    now = pd.to_datetime(datetime.utcnow(), utc=True)
 
     if alerts is None:
         alerts = get_alerts()
