945c6d8d7ccea8d452fbaf2a6b27732dfe997843
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-06-08 09:45:56 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shifthelper',
-    version='1.5.2',
+    version='1.5.3',
     description='a tool for helping people with a FACT night shift',
     url='https://github.com/fact-project/shifthelper',
     author='Dominik Neise, Maximilian Noethe, Sebastian Mueller',
